c183a7885ecd8f39cf68d16a7af569487c081a9f
arimeshinnosuke <e215743@ie.u-ryukyu.ac.jp>
2021-12-19 13:27:06 +0900

    3rd commit

--- report5.java
+++ report5.java
@@ -1,5 +1,13 @@
 public class report5{
     public static void main(String[] args){
+        String str = "百二十三";
+        int value = Integer.parseInt(str);
+        System.out.println(value);
+    }
+}
+
+/*public class report5{
+    public static void main(String[] args){
         try{
         String str = null;
         System.out.println(str.length());
@@ -8,4 +16,4 @@ public class report5{
        System.out.println(e.getMessage());
         }
     }
-}
+}*/
